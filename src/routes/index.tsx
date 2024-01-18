38570fb7fef6d3439f680f02b142a59293c06f2c
SerheyPopov <serheypopov1994@gmail.com>
2024-01-18 18:28:43 +0200

    test_table_second_try

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Main from "../components/pages/Main";
-// import Accounts from "../components/pages/Accounts";
 import Campaigns from "../components/pages/Campaigns";
 import Profiles from "../components/pages/Profiles";
 
